d05e41e8a4886d881be3fa8b8da769686280c9c4
cg <chuganghong@qq.com>
2020-05-30 00:45:28 +0800

    复制复杂链表，时间复杂度O(n)，测试通过

--- linked_list/copy_complicated_linked_list/02/Solution.cpp
+++ linked_list/copy_complicated_linked_list/02/Solution.cpp
@@ -9,9 +9,40 @@
  * random是R2->next。因为，R2->next是R2的克隆
  * 结点。
  * 3.分离出偶数位结点，这些结点组成克隆的结果。
+ * 注意：todo 这是看别人的代码后，自己写出来的。
+ * 花了很多时间。我仍然不能很熟练地写出这些代码。
+ * 理解这思路，也不是很彻底。
  *******************************************/
 #include "Solution.h"
 
-RandomListNode * Solution::Clone(RandomListNode *pHead) {
+RandomListNode *Solution::Clone(RandomListNode *pHead) {
+    if (pHead == NULL) {
+        return NULL;
+    }
 
+    RandomListNode *current = pHead;
+    while (current != NULL) {
+        RandomListNode *node = new RandomListNode(current->label);
+        node->next = current->next;
+        current->next = node;
+        current = node->next;
+    }
+
+    current = pHead;
+    while (current != NULL) {
+        if (current->random != NULL) {
+            current->next->random = current->random->next;
+        }
+        current = current->next->next;
+    }
+
+    current = pHead;
+    RandomListNode *res = pHead->next;
+    while (current->next != NULL) {
+        RandomListNode *tmp = current->next;
+        current->next = tmp->next;
+        current = tmp;
+    }
+
+    return res;
 }
--- linked_list/copy_complicated_linked_list/02/test.cpp
+++ linked_list/copy_complicated_linked_list/02/test.cpp
@@ -1,4 +1,39 @@
 //
 // Created by chugang on 2020/5/29.
 //
+#include <iostream>
+#include "Solution.h"
 
+using namespace std;
+
+void print_linked_list(RandomListNode *pHead);
+
+int main() {
+    Solution solution;
+
+    RandomListNode *node11 = new RandomListNode(1);
+    RandomListNode *node12 = new RandomListNode(2);
+    RandomListNode *node13 = new RandomListNode(3);
+    node11->next = node12;
+    node11->random = node13;
+    node12->next = node13;
+    node12->random = node11;
+    node13->random = NULL;
+    print_linked_list(node11);
+    RandomListNode *node111 = solution.Clone(node11);
+    print_linked_list(node111);
+
+    return 0;
+}
+
+void print_linked_list(RandomListNode *pHead) {
+    RandomListNode *current = pHead;
+    while (current != NULL) {
+        cout << current->label << ",";
+        current = current->next;
+    }
+
+    cout << endl;
+
+    cout << endl << "======================" << endl;
+}
