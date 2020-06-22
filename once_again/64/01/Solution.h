d8755b4b142044ff782d5446f037df6228fc1052
cg <chuganghong@qq.com>
2020-06-22 11:28:15 +0800

    求和，?:改造

---
+++ once_again/64/01/Solution.h
@@ -0,0 +1,15 @@
+//
+// Created by chugang on 2020/6/22.
+//
+
+#ifndef JIAN_ZHI_OFFER_CPP_SOLUTION_H
+#define JIAN_ZHI_OFFER_CPP_SOLUTION_H
+
+
+class Solution {
+public:
+    int sumNums(int n);
+};
+
+
+#endif //JIAN_ZHI_OFFER_CPP_SOLUTION_H
---
+++ once_again/64/01/test.cpp
@@ -0,0 +1,13 @@
+//
+// Created by chugang on 2020/6/22.
+//
+#include <iostream>
+#include "Solution.h"
+
+int main() {
+    using namespace std;
+    Solution solution;
+    int sum1 = solution.sumNums(5);
+    cout << sum1 << endl;
+    return 0;
+}
